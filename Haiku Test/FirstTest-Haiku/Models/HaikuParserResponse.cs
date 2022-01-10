031c045d839793f5cb6d91e401b52da2447e6490
Hakan <confidenthakan@gmail.com>
2022-01-10 15:24:59 +0000

    Add files via upload

---
+++ Haiku Test/FirstTest-Haiku/Models/HaikuParserResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTest_Haiku.Models
+{
+    public class HaikuParserResponse: Response
+    {
+        public int Count { get; set; }
+    }
+}
